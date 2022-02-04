5ff4939c70406c3d54d166872d6ef94380f617ee
Jonathan Jaimes <jonathanguillermojm@ufps.edu.co>
2022-02-04 15:54:46 -0500

    primero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formReactivos';
+
+  constructor(private formBuilder:FormBuilder){
+
+  }
+
+  objeto = {}
+
+  resp1:any
+  resp2:any
+  resp3:any
+  resp4:any
+
+  obtener1(e:any){
+    this.resp1 = e.target.value
+  }
+
+  obtener2(e:any){
+    this.resp2 = e.target.value
+  }
+
+  obtener3(e:any){
+    this.resp3 = e.target.value
+  }
+
+  obtener4(e:any){
+    this.resp4 = e.target.value
+  }
+
+  
+
+  registerForm = this.formBuilder.group({
+    pregunta: [""],
+    resp1: [""],
+    resp2: [""],
+    resp3: [""],
+    resp4: [""],
+    nivel: [""],
+    respCorrect: [false]
+
+  })
+
+
+
+  enviar() {
+    this.objeto = this.registerForm.value
+    console.log(this.objeto)
+  }
+
 }
